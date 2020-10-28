d818749c95c25be08d3910f47a2902b0ecd6ab00
Fernando Fernandes <fernando.fernandes@labtwin.com>
2020-10-28 17:17:12 +0100

    Bump iOS target version to v9.0

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TOCropViewController",
 	defaultLocalization: "en",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v9)],
     products: [
         .library(
             name: "TOCropViewController",
